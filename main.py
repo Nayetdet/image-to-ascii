b25ca827ea5f7e431ef0ef8f12ab3c3c2d4fab51
Nayetdet <122363675+Nayetdet@users.noreply.github.com>
2024-02-15 11:01:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,111 +6,125 @@ from json import load, dump
 from sys import exit
 from os import listdir, system, remove, path
 
+class ConfigurationManager:
+    DEFAULT_CONFIG = {
+        'fps': 24,
+        'width': 80,
+        'height': 40,
+        'reverse_background': False,
+        'print_result': True,
+        'characters': ' _,.:;-~+=*!?/[(&$#@'
+    }
 
-def common_handler():
-    text = convert_to_ascii(image)
-    with open('output.txt', 'w') as file:
-        file.write(text)
+    def __init__(self):
+        self.directory = path.dirname(path.realpath(__file__))
+        self.files = listdir(self.directory)
 
-    if print_result is True:
-        initscr()
-        curs_set(0)
+        self.get_user_config()
+        self.get_user_image()
 
-        mvaddstr(0, 0, text.replace('\n', ''))
-        getch()
-        endwin()
+        if self.reverse_chars is True: self.chars = self.chars[::-1]
+        if not self.height:
+            original_height, original_width = self.image.size
+            self.height = int(self.width * original_height / original_width)
 
+        self.clean_up_previous_files()
 
-def gif_handler():
-    memfile = BytesIO()
-    with ZipFile(memfile, 'w', compression = ZIP_DEFLATED) as zipfile:
-        output = []
-        for frame in range(image.n_frames):
-            image.seek(frame)
-            ascii = convert_to_ascii(image)
+    def check_current_directory(self):
+        expected_files = [path.basename(__file__), 'run.bat', 'output.txt', 'output.zip', 'settings.json', '__pycache__']
+        unexpected_files = [file for file in self.files if not file.endswith(('.gif', '.jpg', '.png')) and file not in expected_files]
+        if unexpected_files:
+            exit('Insira este arquivo em uma pasta vazia.')
 
-            output.append(ascii)
-            zipfile.writestr(
-                f'output{frame + 1}.txt', str.encode(ascii, 'utf-8')
-            )
+    def get_user_config(self):
+        self.check_current_directory()
+        if 'settings.json' not in self.files:
+            with open('settings.json', 'w') as json:
+                dump(self.DEFAULT_CONFIG, json, indent = 4)
+        try:
+            with open('settings.json') as data:
+                user_config = load(data)
+                if (
+                    {k: type(v) for k, v in self.DEFAULT_CONFIG.items()} != {k: type(v) for k, v in user_config.items()}
+                    or not 201 > user_config.get('width', -1) > 0
+                ): exit('O arquivo de configurações foi modificado incorretamente.')
+        except FileNotFoundError: exit('O arquivo de configurações foi deletado.')
+        self.fps, self.width, self.height, self.reverse_chars, self.print_result, self.chars = user_config.values()
+        self.ms = round(1000 / self.fps)
 
-    with open('output.zip', 'wb') as file:
-        file.write(memfile.getvalue())
+    def get_user_image(self):
+        system(f'MODE {self.width}, {self.height}')
+        self.img_path = input('Insira um caminho válido para a imagem: ')
+        try: self.image = Image.open(self.img_path)
+        except: exit('Arquivo inválido.')
 
-    if print_result is True:
-        initscr()
-        timeout(ms)
-        curs_set(0)
-        noecho()
+    def clean_up_previous_files(self):
+        for file in self.files:
+            if file in ('output.zip', 'output.txt'):
+                remove(path.join(self.directory, file))
 
-        while True:
-            for text in output:
-                mvaddstr(0, 0, text.replace('\n', ''))
-                if getch() is ERR: refresh()
-                else:
-                    endwin()
-                    return
+class ASCIIArtGenerator(ConfigurationManager):
+    def __init__(self):
+        super().__init__()
+        self.select_processing_method()
 
+    def handle_image_conversion(self):
+        text = self.convert_to_ascii(self.image)
+        with open('output.txt', 'w') as file:
+            file.write(text)
 
-def convert_to_ascii(image):
-    image  = image.resize((width, height)).convert('L')
-    rawtxt = ''.join([chars[int(color / dividend)] for color in image.getdata()])
-    return '\n'.join([rawtxt[i:(i + width)] for i in range(0, len(rawtxt), width)])
+        if self.print_result is True:
+            initscr()
+            curs_set(0)
 
+            mvaddstr(0, 0, text.replace('\n', ''))
+            getch()
+            endwin()
 
-defaultconfig = {
-    'fps': 24,
-    'width': 80,
-    'height': 40,
-    'reverse_background': False,
-    'print_result': True,
-    'characters': ' _,.:;-~+=*!?/[(&$#@'
-}
+    def handle_gif_conversion(self):
+        memfile = BytesIO()
+        with ZipFile(memfile, 'w', compression = ZIP_DEFLATED) as zipfile:
+            output = []
+            for frame in range(self.image.n_frames):
+                self.image.seek(frame)
+                ascii = self.convert_to_ascii(self.image)
 
-directory = path.dirname(path.realpath(__file__))
-files = listdir(directory)
+                output.append(ascii)
+                zipfile.writestr(
+                    f'output{frame + 1}.txt', str.encode(ascii, 'utf-8')
+                )
 
-defaultfiles = (path.basename(__file__), 'settings.json', 'output.zip', 'output.txt', 'run.bat', '__pycache__')
-invalidfiles = [f for f in files if not f.endswith(('.gif', '.jpg', '.png')) and f not in defaultfiles]
+        with open('output.zip', 'wb') as file:
+            file.write(memfile.getvalue())
 
-if invalidfiles: exit('Insira este arquivo em uma pasta vazia.')
-if 'settings.json' not in files:
-    with open('settings.json', 'w') as json:
-        dump(defaultconfig, json, indent = 4)
+        if self.print_result is True:
+            initscr()
+            timeout(self.ms)
+            curs_set(0)
+            noecho()
 
-try:
-    with open('settings.json') as data:
-        userconfig = load(data)
-        if (
-            {k: type(v) for k, v in defaultconfig.items()} != {k: type(v) for k, v in userconfig.items()}
-            or not 201 > userconfig.get('width', -1) > 0
-        ): exit('O arquivo de configurações foi modificado incorretamente.')
-except FileNotFoundError: exit('O arquivo de configurações foi deletado.')
+            while True:
+                for text in output:
+                    mvaddstr(0, 0, text.replace('\n', ''))
+                    if getch() is ERR: refresh()
+                    else:
+                        endwin()
+                        return
 
-fps, width, height, reverse_chars, print_result, chars = userconfig.values()
-system(f'MODE {width}, {height}')
+    def convert_to_ascii(self, image):
+        image = image.resize((self.width, self.height)).convert('L')
+        raw_text = ''.join([self.chars[int(color / (255 / (len(self.chars) - 1)))] for color in image.getdata()])
+        return '\n'.join([raw_text[i:(i + self.width)] for i in range(0, len(raw_text), self.width)])
 
-imgpath = input('Insira um caminho válido para a imagem: ')
-try: image = Image.open(imgpath)
-except: exit('Arquivo inválido.')
+    def select_processing_method(self):
+        try:
+            match self.img_path[self.img_path.rfind('.') + 1:]:
+                case 'gif': self.handle_gif_conversion()
+                case 'jpg' | 'png': self.handle_image_conversion()
+                case _: exit('Formato não suportado.')
+        except: pass
+        system('cls || clear')
 
-ms = round(1000 / fps)
-
-if not height:
-    original_height, original_width = image.size
-    height = int(width * original_height / original_width)
-
-if reverse_chars is True: chars = chars[::-1]
-dividend = 255 / (len(chars) - 1)
-
-for file in [l for l in files if l in ('output.zip', 'output.txt')]:
-    remove(path.join(directory, file))
-
-try:
-    match imgpath[imgpath.rfind('.') + 1:]: #{
-        case 'gif': gif_handler()
-        case 'jpg' | 'png': common_handler()
-        case _: exit('Formato não suportado.')
-    #}
-except: pass
-system('cls || clear')
+if __name__ == '__main__':
+    ASCIIArtGenerator()
+ 
